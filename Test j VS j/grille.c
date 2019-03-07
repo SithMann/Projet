dbf0caeedce00a15b1d186253569fcae63cc13d3
Clément <clemdub72@gmail.com>
2019-03-07 12:01:33 +0100

    Premier test (make : OK, segmentation fault j vs j)

--- Test j VS j/grille.c
+++ Test j VS j/grille.c
@@ -6,10 +6,10 @@ void afficher_grille(int longueur, int largeur, t_case **grille){
     int i, j;
     for(i=0; i<longueur; i++){
         for(j=0; j<largeur; j++){
-            if(grille[i][j]->piece1!=NULL){
-                afficherPiece(grille[i][j]->piece1);
-                if(grille[i][j]->piece2!=NULL)
-                    afficherPiece(grille[i][j]->piece2);
+            if(grille[i][j].piece1!=NULL){
+                afficherPiece(grille[i][j].piece1);
+                if(grille[i][j].piece2!=NULL)
+                    afficherPiece(grille[i][j].piece2);
             }
             printf("\t");
         }
@@ -19,8 +19,8 @@ void afficher_grille(int longueur, int largeur, t_case **grille){
 
 void ajouter_piece(int x, int y, t_case **grille, t_piece piece){
     //on verrifie si la piece est bloquante avant 
-    if(grille[x][y]->piece1==NULL)
-        grille[x][y]->piece1 = piece;
+    if(grille[x][y].piece1==NULL)
+        grille[x][y].piece1 = &piece;
     else
-        grille[x][y]->piece2 = piece;
+        grille[x][y].piece2 = &piece;
 }
--- Test j VS j/joueur.h
+++ Test j VS j/joueur.h
@@ -7,7 +7,8 @@
 typedef enum e_couleur{RED,BLUE,GREEN,YELLOW,WHITE,PINK}t_couleur;
 
 typedef struct s_joueur{
-    char * pseudo;
+    //char * pseudo;
+    char pseudo[15];//temporaire
     int nbWin;
     int nJoueur;
     t_couleur couleur;

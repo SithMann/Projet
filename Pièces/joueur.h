0ec4a7a3454eef9d75b3d413ff51637cf6a2718c
Noémie Farizon <farizonnoemie@MacBook-Pro-Noemie.local>
2019-03-07 11:15:01 +0100

    suite doxygene

--- Pièces/joueur.h
+++ Pièces/joueur.h
@@ -5,12 +5,20 @@
 * \file joueur.h
 * \author Mathis Despres
 * \date 7 mars 2019
-*
-* 
 */
 
+/**
+* \typedef enum
+* \enum e_couleur
+* \brief structure qui regroupe l'ensemble des couleurs possible que le joueur peut choisir pour ses pieces
+* */
 typedef enum e_couleur{RED,BLUE,GREEN,YELLOW,WHITE,PINK}t_couleur;
 
+/**
+* \typedef struct
+* \struct s_joueur
+* \brief structure qui permet de sauvegarder les informations relatives au joueur tel que le score...
+* */
 typedef struct s_joueur{
     char * pseudo;
     int nbWin;
--- grille.c
+++ grille.c
@@ -6,15 +6,22 @@
 * \file grille.c
 * \author Noemie Farizon
 * \date 5 mars 2019
-*
-* 
 */
 
+/**
+* \typedef struct
+* \struct s_case
+* \brief structure qui permet de stocker le contenue d'une case de la grille(nombre de pieces, type...)
+*/
 typedef struct s_case{
     t_piece *piece1; //pointeur sur la premiere piece presente dans la case 
     t_piece *piece2; //vide si la première pièce est de type bloquante 
 }t_case;
 
+/**
+* \param entier longueur, entier largeur, matrice grille de type t_case
+* \return la fonction ne retourne rien
+* */
 void afficher_grille(int longueur, int largeur, t_case **grille){
     int i, j;
     for(i=0; i<longueur; i++){
@@ -30,6 +37,10 @@ void afficher_grille(int longueur, int largeur, t_case **grille){
     }
 }
 
+/**
+* \param entier x, entier y, matrice grille de type t_case, piece de type t_piece
+* \return la fonction ne retourne rien
+* */
 void ajouter_piece(int x, int y, t_case **grille, t_piece piece){
     //on verrifi si la piece est bloquante avant 
     if(grille[x][y]->piece1==NULL)

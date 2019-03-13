e94b5dd393c62f4f4a1233efe6a55a92bef53485
Despres Mathis <s171162@ic2s120-05.univ-lemans.fr>
2019-03-13 13:11:57 +0100

    commit

--- Pièces/test_piece.c
+++ Pièces/test_piece.c
@@ -4,14 +4,18 @@
 #include "piece.h"
 
 int main(){
+
     t_joueur joueur;
     joueur.pseudo = "test";
     joueur.nbWin = 0;
     joueur.nJoueur = 1;
     joueur.couleur = RED;
+
     t_piece piece = creerPiece(1,10, joueur);
+
     printf("Une piece : ");
     afficherPiece(&piece);
     printf("\n");
+
     return 0;
 }

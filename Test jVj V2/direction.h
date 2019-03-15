fa28b02b3899893e0f11a0307edd5243824f49ad
Dubois Clement <s174109@ic2s124-12.univ-lemans.fr>
2019-03-15 16:29:40 +0100

    début direction.h

--- Test jVj V2/direction.h
+++ Test jVj V2/direction.h
@@ -1,11 +1,21 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef enum { NORD, NORDEST, EST, SUDEST, SUD, SUDOUEST, OUEST, NORDOUEST } t_direction;
 const int NB_DIRECTION=8;
 
-int direction_avancer(int i, int j, t_direction dir, int nb_pas, int *ni, int*nj);
+int direction_avancer(int i, int j, t_direction dir, int nb_pas, int *ni, int*nj){ //modifier les valeurs de l'appelant pour *ni, *nj
+    int i;
+    dir = direction_debut();
+    for(i = 0; i < NB_DIRECTION; i++){
+        //traitement si même couleur et pas hors matrice on continue
+        dir = direction_suivante(dir);
+    }
+}
 
 t_direction direction_suivante(t_direction dir);
 
-t_direction direction_debut(){ return NORD;}
+t_direction direction_debut(){
+    return NORD;
+}
 

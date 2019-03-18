8ab69ae2496689efdffda2adccd757d9bf61f598
noeemie <48018645+noeemie@users.noreply.github.com>
2019-03-18 16:15:13 +0100

    Update direction.c

--- Test jVj V2/direction.c
+++ Test jVj V2/direction.c
@@ -3,18 +3,25 @@
 #include "jeu.h"
 #include "direction.h"
 
-const int NB_DIRECTION=8;
+/**
+* \file direction.c
+* \author Clement Dubois
+* \date 16 mars 2019
+* \version 1
+*/
 
+/**
+* \fn direction_avancer
+* \param deux entiers i et j (indicage), une direction dir, un entier pour le nombre de pas, un pointeur sur une grille
+* \return la fonction retourne un entier  
+*/
 //modifie les valeurs de l'appelant pour *ni, *nj
 int direction_avancer(int i, int j, t_direction dir, int nb_pas/*, int *ni, int*nj*/, t_grille * grille){
     int k, l;
     int count = 0;
-    fprintf(stderr, "test");//n'affiche rien
     dir = direction_debut();
-    fprintf(stderr, "fonction direction_debut");
     t_couleur couleur1 = lire_couleur_joueur_slot(i, j, 1, grille);
     t_couleur couleur2 = lire_couleur_joueur_slot(i, j, 2, grille);
-    fprintf(stderr, "Test après affctation couleur");
     //traitement si même couleur et pas hors matrice on continue
     for(k = 0; k < NB_DIRECTION; k++){
         for(l = 1; l <= nb_pas; l++){
@@ -59,20 +66,26 @@ int direction_avancer(int i, int j, t_direction dir, int nb_pas/*, int *ni, int*
                             if(count == nb_pas) return 1;
                          }
                          break;
-                default : 
-                    fprintf(stderr, "Erreur");
-                    break;
             }
         }
         dir = direction_suivante(dir);
     }
-    return 0;
 }
 
+/**
+* \fn direction_suivante
+* \param une direction dir 
+* \return la fonction retourne une direction
+*/
 t_direction direction_suivante(t_direction dir){
     return dir + 1;
 }
 
+/**
+* \fn direction_debut
+* \param la fonction ne prend pas de parametres
+* \return la fonction retourne une direction 
+*/
 t_direction direction_debut(){
     return NORD;
 }

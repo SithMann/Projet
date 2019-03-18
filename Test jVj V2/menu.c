2e0a254b38d7a3537eed361f794258bf04b3d10c
Clément <clemdub72@gmail.com>
2019-03-18 08:16:27 +0100

    correction

--- Test jVj V2/menu.c
+++ Test jVj V2/menu.c
@@ -88,10 +88,6 @@ void menu_joueur(int * nb_ligne, int * nb_colonne){
     if((res = ((int)nb_piece_c + (int)nb_piece_p + (int)nb_piece_b)) != (int)nb_case)
         nb_piece_b++;
 
-    /*Comme c'est un calcul de float, j'ajoute un si c'est arrondi en dessous*/
-    if((nb_piece_b + nb_piece_c + nb_piece_p) != (int)nb_case)
-        nb_piece_b++;
-
     /*Création de la grille avec le nb de lignes et colonnes  calculées*/
     t_grille * grille = creer_grille(*nb_ligne, *nb_colonne, sizeof(t_case*));
 

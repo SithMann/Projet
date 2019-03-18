ed6a62bd9a2bdbf9598dd757dcd4eb9afbae8b68
noeemie <48018645+noeemie@users.noreply.github.com>
2019-03-18 16:14:39 +0100

    Update direction.h

--- Test jVj V2/direction.h
+++ Test jVj V2/direction.h
@@ -3,9 +3,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "grille.h"
+#include "jeu.h"
 
+/**
+* \file direction.h
+* \author Clement Dubois
+* \date 16 mars 2019
+* \version 1
+*/
+
+/**
+* \typedef enumeration
+* \enum t_direction
+* \brief contient l'ensemble des directions possibles 
+*/
 typedef enum { NORD, NORDEST, EST, SUDEST, SUD, SUDOUEST, OUEST, NORDOUEST } t_direction;
+const int NB_DIRECTION=8;
 
 int direction_avancer(int i, int j, t_direction dir, int nb_pas/*, int *ni, int*nj*/, t_grille * grille);
 t_direction direction_suivante(t_direction dir);

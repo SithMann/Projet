3d020c855ad8323916177f6fdebb7d3622d48b36
Clément <clemdub72@gmail.com>
2019-03-26 12:01:50 +0100

    SDL premier pas sur le menu

--- SDL/test_sdl.c
+++ SDL/test_sdl.c
@@ -3,18 +3,58 @@
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_image.h>
 
+/*Faire une fonction qui fait une colonne en fonction du nombre de ligne choisies*/
+void creer_grille(int nb_ligne, int nb_colonne, SDL_Renderer *renderer, SDL_Texture *image_tex){
+	SDL_Rect imgDestRect = {200,100,100,100}; //{point de départ en x, point de départ en y, largeur image, longueur image}
+	for(int i = 0; i < nb_colonne; i++){
+		for(int j = 1; j <= nb_ligne; j++){
+			imgDestRect.y += imgDestRect.h;
+			SDL_RenderCopy(renderer, image_tex, NULL, &imgDestRect);
+		}
+		imgDestRect.x += imgDestRect.h;
+		imgDestRect.y = 100;
+	}
+}
+
+/*Créer une structure pour le bouton avec ses tailles et une autre pour le menu où on utilise un tableau de bouton*/
+#define NB_BOUTON 3
+static char* NOM_BOUTON[NB_BOUTON] = {"JoueurVsOrdi", "JoueurVsJoueur", "Quitter"};
+
+void creer_bouton(int x, int y, SDL_Surface *bouton, SDL_Renderer *renderer){
+	int i;
+	TTF_Font *police = TTF_OpenFont("Sketch 3D.otf", 50);
+	SDL_Surface *texte;
+	SDL_Rect txtDestRect, button;
+	SDL_Color couleurNom = {0, 0, 0};
+
+	txtDestRect.x = x - 200;
+	txtDestRect.y = y - 100;
+	bouton = SDL_CreateRGBSurface(0, txtDestRect.y, txtDestRect.x, 32, 0, 0, 0, 255);
+	for(i = 0; i < NB_BOUTON; i++){
+		SDL_FillRect(bouton, NULL, SDL_MapRGB(bouton->format, 255, 255, 0));
+		SDL_BlitSurface(bouton, button, bouton, NULL);
+		texte = TTF_RenderUTF8_Blended(police, NOM_BOUTON[i], couleurNom);
+		SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+		SDL_Texture *texte_tex = SDL_CreateTextureFromSurface(renderer, texte); 
+		SDL_QueryTexture(texte_tex, NULL, NULL, &(txtDestRect.w), &(txtDestRect.h));
+		SDL_RenderCopy(renderer, texte_tex, NULL, &txtDestRect);
+		txtDestRect.y += 100;
+	}
+	TTF_CloseFont(police);
+}
+
 int main(int argc, char** argv){
-	
 	int nb_ligne = 5;
 	int nb_colonne = 7; 
 
     //Le pointeur vers la fenetre
 	SDL_Window* pWindow = NULL;
+	int w_pWindow, h_pWindow;
 	//Le pointeur vers la surface incluse dans la fenetre
-    SDL_Surface *texte=NULL, *image=NULL; 
-	SDL_Surface *optimizedImage=NULL;
+    SDL_Surface *texte=NULL, *image=NULL, *image2 = NULL, *ecran = NULL;
+	SDL_Surface *bouton = NULL;
 	SDL_Renderer *renderer=NULL;
-	SDL_Rect txtDestRect,imgDestRect;
+	SDL_Rect txtDestRect,imgDestRect, coordsouris;
 
 	// Le pointeur vers notre police
 	TTF_Font *police = NULL;
@@ -41,11 +81,13 @@ int main(int argc, char** argv){
 												  1080,
 												  SDL_WINDOW_SHOWN|SDL_WINDOW_RESIZABLE);
 
+	SDL_GetWindowSize(pWindow, &w_pWindow, &h_pWindow);
+	printf("x main : %d, y main : %d\n", w_pWindow, h_pWindow);
+
 	if(!pWindow){
 		fprintf(stderr, "Erreur à la création de la fenetre : %s\n", SDL_GetError());
 		exit(EXIT_FAILURE);
 	}
-
 
 	renderer = SDL_CreateRenderer(pWindow, -1, SDL_RENDERER_ACCELERATED);
 	if(renderer == NULL){
@@ -58,6 +100,8 @@ int main(int argc, char** argv){
 		exit(EXIT_FAILURE);
 	}
 	texte = TTF_RenderUTF8_Blended(police, "PUISSANCE 4++", couleurTitre);
+
+	/*Test si erreur sur la création du texte*/
 	if(!texte){
 		fprintf(stderr, "Erreur à la création du texte : %s\n", SDL_GetError());
 		exit(EXIT_FAILURE);
@@ -75,27 +119,28 @@ int main(int argc, char** argv){
 	txtDestRect.y = 100;
 	SDL_QueryTexture(texte_tex, NULL, NULL, &(txtDestRect.w), &(txtDestRect.h));
 
-	//SDL_RWops *rwop=SDL_RWFromFile("case.png", "rb");
-	SDL_Texture *image_tex; 
-	// load sample.png into image
 	
-	image=IMG_Load("case.png");
+	SDL_Texture *image_tex; 
+	SDL_Texture *image2_tex;
+	// load sample.png into image
+	image2 = IMG_Load("case.png");
+	image=IMG_Load("transparent.gif");
+
+	/*Test si l'image a bien été get*/
 	if(!image) {
 		printf("IMG_LoadPNG_RW: %s\n", IMG_GetError());
 	}
-	/*if( image != NULL ){
-        //Création de l'image optimisée
-        optimizedImage = SDL_DisplayFormat( image );
-	}*/
-	
+
 	image_tex = SDL_CreateTextureFromSurface(renderer, image);
+	image2_tex = SDL_CreateTextureFromSurface(renderer, image2);
+	
 	if(!image_tex){
 		fprintf(stderr, "Erreur à la création du rendu de l'image : %s\n", SDL_GetError());
 		exit(EXIT_FAILURE);
 	}
 			
 	SDL_FreeSurface(image); /* on a la texture, plus besoin de l'image */
-	int i = 0;
+	//int i = 0;
 	if( pWindow ){
 		int running = 1; 
 		while(running) { 
@@ -104,6 +149,13 @@ int main(int argc, char** argv){
 				switch(e.type) { 
 					case SDL_QUIT: running = 0; 
 					break; 
+					case SDL_MOUSEBUTTONUP:
+        				if (e.button.button == SDL_BUTTON_LEFT){ /* On arrête le programme si on a fait un clic gauche */
+							coordsouris.x = e.button.x;
+							coordsouris.y = e.button.y;
+						}
+						printf("%i, %i \n", coordsouris.x, coordsouris.y);
+							break;
 					case SDL_WINDOWEVENT:
 						switch(e.window.event){
 							case SDL_WINDOWEVENT_EXPOSED:
@@ -111,29 +163,28 @@ int main(int argc, char** argv){
 							case SDL_WINDOWEVENT_RESIZED:
 							case SDL_WINDOWEVENT_SHOWN:
 								/* Le fond de la fenêtre sera blanc */
+								SDL_RenderClear(renderer);
                                 SDL_SetRenderDrawColor(renderer, 44, 75, 111, 255);
+								//SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
 								SDL_RenderClear(renderer);
                                 
                                 /* Ajout du texte en noir */
+								
                                 SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
                                 SDL_RenderCopy(renderer, texte_tex, NULL, &txtDestRect);
 								
                                 /* Ajout de la seconde image à une certaine position */
                                 imgDestRect.x = 10;
-								imgDestRect.y = 110;
+								imgDestRect.y = 110;									
 								SDL_QueryTexture(image_tex, NULL, NULL, &(imgDestRect.w), &(imgDestRect.h));
-								//SDL_RenderCopy(renderer, image_tex, NULL, &imgDestRect);
+								SDL_RenderCopy(renderer, image_tex, NULL, &imgDestRect);
 								
-                                /* Ajout de la seconde image à une autre position */
-								for(int i = 0; i < nb_colonne; i++){
-									imgDestRect.x += 75;
-									imgDestRect.y = 110;
-									for(int j = 0; j < nb_ligne; j++){
-										imgDestRect.y += 75;
-										SDL_RenderCopy(renderer, image_tex, NULL, &imgDestRect);
-									}
-								}
-								
+                                //Ajout de la seconde image à une autre position 
+								//creer_grille(nb_ligne, nb_colonne, renderer, image_tex);
+		
+								//création des boutons du premier menu
+								creer_bouton(w_pWindow / 2, h_pWindow / 2, bouton, renderer);
+
                                 /* On fait le rendu ! */
                                 SDL_RenderPresent(renderer);
 								

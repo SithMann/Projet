3e2a6fb8900b22d62b04665befb2226e4edf1cdc
SithMann <38142935+SithMann@users.noreply.github.com>
2019-04-11 16:03:11 +0200

    Elagage

--- Test jVj V2/strat_g.h
+++ Test jVj V2/strat_g.h
@@ -8,8 +8,8 @@
 int coup_gagnant(t_grille * grille, t_joueur *joueur, int nJetons);
 int jouer_coup_gagnant(t_grille * grille, t_joueur *joueur, int nJetons);
 void dejouer(t_piece piece, int nbCol, t_grille * grille, t_joueur* joueur);
-int adversaire(t_grille * grille, t_joueur * joueur, int num_joueur, int profondeur, int prof_max, int nb_joueur, int num_ordi, int nJetons);
-int ordi(t_grille * grille, t_joueur * joueur, int num_joueur, int profondeur, int prof_max, int nb_joueur, int num_ordi, int nJetons);
+int adversaire(t_grille * grille, t_joueur * joueur, int num_joueur, int profondeur, int prof_max, int nb_joueur, int num_ordi, int nJetons, int alpha);
+int ordi(t_grille * grille, t_joueur * joueur, int num_joueur, int profondeur, int prof_max, int nb_joueur, int num_ordi, int nJetons, int beta);
 void tour_ordi(t_grille * grille, t_joueur * joueur, int num_joueur, int prof_max, int nb_joueur, int nJetons);
 int evaluation(t_grille *grille, t_joueur * joueur, int num_joueur, int nb_joueur, int nJetons);
 
